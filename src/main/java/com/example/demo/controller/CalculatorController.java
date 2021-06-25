4cdb193f4c395a7849ef254cdf7b7d028983c161
chenjinxuan <360088940@qq.com>
2021-06-25 16:34:37 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author: JJJJ
+ * @date:2021/6/24 9:22
+ * @Description: TODO
+ */
+@RestController
+public class CalculatorController {
+
+
+    @GetMapping("/add")
+    public void getReport(Integer num1,Integer num2,String method){
+        if (num1 == null){
+            System.out.println("数值1不能为空");
+
+        }else if (num2 == null){
+            System.out.println("数值2不能为空");
+
+        }else {
+            if (method.equals("add")){
+                System.out.println(num1+num2);
+            }
+            if (method.equals("del")){
+                System.out.println(num1-num2);
+            }
+            if (method.equals("mul")){
+                System.out.println(num1*num2);
+            }
+            if (method.equals("div")){
+                if (num2 == 0){
+                    System.out.println("被除数不能为0");
+                }else
+                    System.out.println(num1/num2);
+            }
+        }
+
+    }
+}
