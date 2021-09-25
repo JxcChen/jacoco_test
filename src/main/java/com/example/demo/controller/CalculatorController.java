957e49665c76fc9d24712e0614cef94c5106b432
chnjx <360088940@qq.com>
2021-09-25 21:02:32 +0800

    修改add1

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -22,14 +22,14 @@ public class CalculatorController {
         Integer num1 = param.getNum1();
         Integer num2 = param.getNum2();
         if (num1 == null){
-            System.out.println("数值1不能为空!!");
+            System.out.println("数值1不能为空!!！！！");
 
         }else if (num2 == null){
-            System.out.println("数值2不能为空啊！！dev分支测试");
+            System.out.println("数值2不能为空啊！！！！dev分支测试");
 
         }else {
             if (method.equals("add")){
-                System.out.println(num1+num2+"修改");
+                System.out.println(num1+num2+"修改！！！");
             }
             if (method.equals("del")){
                 System.out.println(num1-num2);
